dfee87c5ed91beb2a66c487cbcc7a59880d28bae
Cory Anderson <cory.anderson6@yahoo.com>
2023-03-05 22:18:12 -0600

    added clear button

--- client.js
+++ client.js
@@ -1,2 +1,15 @@
 console.log('Hello World');
 
+window.onload = function () {
+
+    let screen = document.getElementById('screen');
+
+    let buttons = document.querySelectorAll("button[id^='number']");
+
+    for(let i =0; i < buttons.length; i++) {
+        buttons[i].addEventListener("click", function() {
+            screen.value += this.textContent;
+        });
+    }
+}
+

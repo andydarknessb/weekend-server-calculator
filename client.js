aa46fce90ac6a88b8b1bb2a9f0c002db893d2233
Cory Anderson <cory.anderson6@yahoo.com>
2023-03-06 21:00:28 -0600

    Updating buttons

--- client.js
+++ client.js
@@ -1,40 +1,81 @@
 console.log('Hello World');
 
-window.onload = function () {
+const screen = document.getElementById('screen');
 
-    let screen = document.getElementById('screen');
-
-    let buttons = document.querySelectorAll("button[id^='number']");
+    const buttons = document.querySelectorAll("button:not(#clear):not(#backspace):not(#power):not(#divide):not(#multiply):not(#subtract):not(#add):not(#decimal):not(#equals)");
 
     for(let i =0; i < buttons.length; i++) {
         buttons[i].addEventListener("click", function() {
             screen.value += this.textContent;
         });
-    }
+    
 }
 
-let clearButton = document.getElementById("clear");
+const clearButton = document.getElementById("clear");
 
 clearButton.addEventListener("click", function() {
     screen.value = "";
 });
 
-let backspaceButton = document.getElementById("backspace");
+const backspaceButton = document.getElementById("backspace");
 
 backspaceButton.addEventListener("click", function(){
     screen.value = screen.value.slice(0,-1);
 });
 
-let powerButton = document.getElementById("power");
+const powerButton = document.getElementById("power");
 
 powerButton.addEventListener("click", function() {
     screen.value += "**";
 
 });
 
-let equalsButton = document.getElementById("equals");
-    equalsButton.addEventListener("click", function() {
-        screen.value = eval(screen.value);
-    });
+const divideButton = document.getElementById("divide");
+
+divideButton.addEventListener("click", function(){
+    screen.value += "/";
+});
+
+const multiplyButton = document.getElementById("multiply");
+
+multiplyButton.addEventListener("click", function() {
+    screen.value += "*"
+});
+
+	// Get the subtract button
+	const subtractButton = document.getElementById("subtract");
+
+	// Add event listener to the subtract button
+	subtractButton.addEventListener("click", function() {
+		screen.value += "-";
+	});
+
+	// Get the add button
+	const addButton = document.getElementById("add");
+
+	// Add event listener to the add button
+	addButton.addEventListener("click", function() {
+		screen.value += "+";
+	});
+
+	// Get the decimal button
+	const decimalButton = document.getElementById("decimal");
+
+	// Add event listener to the decimal button
+	decimalButton.addEventListener("click", function() {
+		if (!screen.value.includes(".")) {
+			screen.value += ".";
+		}
+	});
+
+	// Get the equals button
+	const equalsButton = document.getElementById("equals");
+
+	// Add event listener to the equals button
+	equalsButton.addEventListener("click", function() {
+        console.log("screen.value:", screen.value);
+		screen.value = eval(screen.value);
+	});
+
 
     
